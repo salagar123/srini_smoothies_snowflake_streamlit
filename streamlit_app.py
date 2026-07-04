17ecf7f8b0ee602695aacfa9f249b2de64fac418
salagar123 <56902768+salagar123@users.noreply.github.com>
2026-07-03 23:32:40 -0400

    Remove write statement for sf_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if ingredients_list:
 import requests  
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-st.write(sf_df)
+
